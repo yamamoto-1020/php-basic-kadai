a58be9fa5a02642aa301f71ab95fa8c7a530b416
yamamoto-1020 <yu_yamamoto1020@outlook.jp>
2025-05-10 07:13:14 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+      $list1 = ['name' => 'onion','price' => '200','weight' => '160'];
+      print_r($list1);
+    ?>
+  </p>
+</body>
+
+</html>
